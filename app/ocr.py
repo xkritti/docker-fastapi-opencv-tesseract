92d69e4fef0ca876b3a976a5ecb0edb27fc314a3
Krittamet_CoE#15 <krittamet0404@gmail.com>
2021-09-30 14:11:16 +0700

    Update ocr.py

--- app/ocr.py
+++ app/ocr.py
@@ -2,25 +2,7 @@ import pytesseract
 import cv2
 import numpy as np
 
-
-async def read_image(img_path, lang='eng'):
-    """
-    Performs OCR on a single image
-    :img_path: str, path to the image file
-    :lang: str, language to be used while conversion (optional, default is english)
-    Returns
-    :text: str, converted text from image
-    """
-
-    try:
-        """ data from image is String """
-        return {"error": False, "res": pytesseract.image_to_string(img_path, lang=lang)}
-    except Exception as e:
-        print(e)
-        return {"error": True, "res": "[ERROR] Unable to process file: {0}".format(img_path)}
-
-
-async def test(img_path):
+async def read_image(img_path):
 
     try:
         room = ''
